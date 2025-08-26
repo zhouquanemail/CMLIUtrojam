c4d74df62fb9ad69ccaa7e3bc381b8db2d9dfe38
zhouquanemail <zhouquanemail@gmail.com>
2025-08-27 00:13:36 +0800

    准备混淆--vless无需proxyip的nat64套壳版 (推荐使用).js

--- Vless_workers_pages/vless无需proxyip的nat64套壳版 (推荐使用).js
+++ Vless_workers_pages/vless无需proxyip的nat64套壳版 (推荐使用).js
@@ -1,7 +1,7 @@
 //nat64自动填充proxyip，无需且不支持proxyip设置
 import { connect } from "cloudflare:sockets";
 const WS_READY_STATE_OPEN = 1;
-let userID = "86c50e3a-5b87-49dd-bd20-03c7f2735e40";
+let userID = "f4865b7a-2897-4f65-9d59-bd9a369194fe";
 const cn_hostnames = [''];
 let CDNIP = '\u0077\u0077\u0077\u002e\u0076\u0069\u0073\u0061\u002e\u0063\u006f\u006d\u002e\u0073\u0067'
 // http_ip
